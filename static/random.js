f7f6e0850ce1e3af5186e50ad30bc63afdcbcd8c
lee-insu <carpediem7760@gmail.com>
2021-04-07 19:01:19 +0900

    random js

--- static/random.js
+++ static/random.js
@@ -1,54 +1,59 @@
+let coinList = [	["비트코인"],
+                    ["인수코인"],
+                    ["알트코인"],
+                    ["비트코인"],
 
-let coinList = [
-    ["1","비트코인"],
-    ["2","알트코인"],
-    ["3","인수코인"],
-    ["4","인수코인"],
-    ["5","인수코인"],
-    ["6","인수코인"],
-    ["7","인수코인"]
 ];
+
 
 function random(data) {
 
-    lotCnt=1;
-    datalotCnt = data.length;
-    coinSelect = new Array(datalotCnt);
-    coinSelect[0] = 0;
-   
+    lotCnt= 1;	//추첨인원
+    datalotCnt = data.length;	//자료수
+    coinPick = new Array(datalotCnt);	//당첨번호
+    coinPick[0] = 0;
 
-    for(let i=0; i<=lotCnt; i++) {
-        flag = 0;
-        while(flag === 0) {
-            result = Math.floor(Math.random() * datalotCnt);
+    for (let i=0; i<=lotCnt; i++)
+    {
+      flag = 0;
+
+      while(flag === 0) {
+        result=Math.floor(Math.random() * datalotCnt);
 
         for(let j=0; j<=i; j++) {
-            if(result === coinSelect[j]) {
-                flag = 0;
-                break;
-            }else {
-                flag= 1 ;
-            }
-          }
-    }
-    coinSelect[i] = result;
-}
-
-temp = 0;
-
-for(let i=0; i<=lotCnt-1; i++) {
-    for(let j=i+1; j<=lotCnt; j++) {
-        if(coinSelect[i] > coinSelect[j]) {
-            temp = coinSelect[i];
-            coinSelect[i] = coinSelect[j];
-            coinSelect[j] = temp;
+           if (result == coinPick[j]) {
+            flag = 0;
+            break;
+           } else {
+            flag = 1;
+           }
         }
-    }
-}
+      }
 
-for(let i =1; i<=lotCnt; i++ ) {
-    console.log(data[coinList[i][0]]);
-}
+      coinPick[i] = result;
+    }
+
+    temp = 0;
+
+    for (let i=0; i<=lotCnt-1; i++)  {
+      for (let j=i+1; j<=lotCnt; j++) {
+        if (coinPick[i] > coinPick[j]) {
+          temp= coinPick[i];
+          coinPick[i] = coinPick[j];
+          coinPick[j] = temp;
+        }
+      }
+    }
+
+    
+    for (let i=1; i<=lotCnt; i++) 
+    {
+     console.log(data[coinPick[i]][0]);
+     const coinShow = document.querySelector(".kk");
+     coinShow.innerText = `${data[coinPick[i]][0]}`;
+    
+    }
+
 }
 
 
