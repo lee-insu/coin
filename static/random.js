53656b46eda31f8adae01c403bb55bb06f8274e9
lee-insu <carpediem7760@gmail.com>
2021-04-07 18:20:51 +0900

    js

--- static/random.js
+++ static/random.js
@@ -1,7 +1,12 @@
+
 let coinList = [
-    [1,"비트코인"],
-    [2,"알트코인"],
-    [3,"인수코인"]
+    ["1","비트코인"],
+    ["2","알트코인"],
+    ["3","인수코인"],
+    ["4","인수코인"],
+    ["5","인수코인"],
+    ["6","인수코인"],
+    ["7","인수코인"]
 ];
 
 function random(data) {
@@ -10,7 +15,7 @@ function random(data) {
     datalotCnt = data.length;
     coinSelect = new Array(datalotCnt);
     coinSelect[0] = 0;
-    console.log(lotCnt);
+   
 
     for(let i=0; i<=lotCnt; i++) {
         flag = 0;
@@ -32,7 +37,7 @@ function random(data) {
 temp = 0;
 
 for(let i=0; i<=lotCnt-1; i++) {
-    for(let j=i+1; j<=lotCnt; J++) {
+    for(let j=i+1; j<=lotCnt; j++) {
         if(coinSelect[i] > coinSelect[j]) {
             temp = coinSelect[i];
             coinSelect[i] = coinSelect[j];
@@ -42,5 +47,8 @@ for(let i=0; i<=lotCnt-1; i++) {
 }
 
 for(let i =1; i<=lotCnt; i++ ) {
-    document.getElementsByClassName('kk').innerHTML(data[coinSelect[i]][1])
+    console.log(data[coinList[i][0]]);
 }
+}
+
+
